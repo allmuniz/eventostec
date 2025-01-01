1afd42c7dd152de8120d77d1f02d761e6f52f83f
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-01-01 10:14:04 -0300

    feat: update list all events with pagination

--- src/main/java/com/project/api/service/EventService.java
+++ src/main/java/com/project/api/service/EventService.java
@@ -3,15 +3,20 @@ package com.project.api.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.project.api.domain.event.Event;
 import com.project.api.domain.event.EventRequestDTO;
+import com.project.api.domain.event.EventResponseDTO;
 import com.project.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -45,6 +50,14 @@ public class EventService {
         eventRepository.save(newEvent);
 
         return newEvent;
+    }
+
+    public List<EventResponseDTO> getEvents(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = eventRepository.findAll(pageable);
+        return eventsPage.map(event -> new EventResponseDTO(event.getId(), event.getTitle(), event.getDescription(), event.getDate(),"","",event.getRemote(), event.getEventUrl(), event.getImgUrl()))
+                .stream().toList();
+
     }
 
     private String uploadImg(MultipartFile multipartFile) {
