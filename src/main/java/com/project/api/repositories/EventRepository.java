c0913eb2ddc5fcde8bcc5388f34204e7beb3c7c1
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-01-01 09:35:52 -0300

    feat: creating repositories

---
+++ src/main/java/com/project/api/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.project.api.repositories;
+
+import com.project.api.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
